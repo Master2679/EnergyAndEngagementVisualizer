6b51dd5ab43b90194845e7893d142074f0b3e56f
ultimate-grass-cutter <kitfud@gmail.com>
2023-10-24 14:12:01 -0700

    Commit no. 2 - activity log

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
 
     # Instantiate dataframe
     if 'data' not in st.session_state:
-        st.session_state.data = pd.DataFrame(columns=['Activity', 'Energy', 'Engagement', 'Time'])
+        st.session_state.data = pd.DataFrame(columns=['Date', 'Activity', 'Energy', 'Engagement', 'Time'])
 
     # Main body
     st.title('Energy and Engagement Visualizer')
@@ -23,6 +23,7 @@ def main():
         # Entry parameters
         st.write('Please enter activity information.')
         activity = st.text_input('Activity name')
+        date = st.date_input('Date')
         energy = st.slider('Enter energy level', 0, 5)
         engagement = st.slider('Enter engagement level', 0, 5)
         time = st.number_input('Enter elapsed time')
@@ -30,11 +31,22 @@ def main():
         # Submission
         submitted = st.form_submit_button("Submit")
         if submitted and activity:
-            data_entry = {'Activity': activity, 'Energy': energy, 'Engagement': engagement, 'Time': time}
+            data_entry = {'Date': str(date), 'Activity': activity, 'Energy': energy, 'Engagement': engagement, 'Time': time}
             st.session_state.data = pd.concat([st.session_state.data, pd.DataFrame([data_entry])], ignore_index=True)
     
-    # Display dataframe
+    # Display activity log
     st.header('Activity Log:')
+    
+    grouped_dataframe = st.session_state.data.groupby('Date')
+    for date, group in grouped_dataframe:
+        with st.expander(str(date), expanded=True):
+            for index, row in group.iterrows():
+                st.subheader(row['Activity'])
+                st.write(f"- Energy Level: {row['Energy']}\n- Engagement: {row['Engagement']}\n- Time Spent: {row['Time']} hr(s)")
+                st.divider()
+    
+    # Complete log
+    st.header("Complete dataframe")
     st.write(st.session_state.data)
 
 if __name__ == "__main__":
